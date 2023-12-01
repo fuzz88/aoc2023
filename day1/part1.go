162269c15fff40f4d05c10d71590422b42a6d481
fuzz88 <ivan@oschepkov.ru>
2023-12-01 20:30:18 +0800

    add day1/part1

---
+++ day1/part1.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+	sum := 0
+	t1 := -1
+	t2 := -1
+	for _, k := range s {
+
+		if k == 10 {
+			if t2 == -1 {
+				t2 = t1
+			}
+			if t1 == -1 && t2 == -1 {
+				continue
+			}
+			fmt.Printf("%v%v\n", t1, t2)
+			sum = sum + t1*10 + t2
+			fmt.Printf("%v\n", sum)
+			t1 = -1
+			t2 = -1
+		}
+		if (47 < k) && (k < 48+10) {
+			fmt.Printf("%v\n", k-48)
+			if t1 == -1 {
+				t1 = int(k - 48)
+				continue
+			}
+
+			t2 = int(k - 48)
+
+		}
+	}
+	if t2 == -1 {
+		t2 = t1
+	}
+	fmt.Printf("%v%v\n", t1, t2)
+    fmt.Printf("%v\n", sum+t1*10+t2)
+}
