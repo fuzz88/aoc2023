c16ea57fc8f6684971178f6dae39c67ab327e0f2
fuzz88 <ivan@oschepkov.ru>
2023-12-07 23:17:25 +0800

    day3 finished

--- day3/part2.go
+++ day3/part2.go
@@ -4,29 +4,14 @@ import (
 	"fmt"
 	"math"
 	"os"
-	"sync"
 )
 
-var wg sync.WaitGroup
-
-type SafeCounter struct {
-	mu sync.Mutex
-	v  int
+type Gear struct {
+	i   int
+	num int
 }
 
-func (c *SafeCounter) Inc(n int) {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	c.v = c.v + n
-	c.mu.Unlock()
-}
-
-func (c *SafeCounter) Value() int {
-	c.mu.Lock()
-	// Lock so only one goroutine at a time can access the map c.v.
-	defer c.mu.Unlock()
-	return c.v
-}
+var gears []*Gear
 
 func main() {
 	raw_data, err := os.ReadFile("test2.txt")
@@ -34,31 +19,42 @@ func main() {
 		panic(err)
 	}
 	// fmt.Println(raw_data)
-	sum := SafeCounter{v: 0}
+
 	line_start := 0
 	line_count := 0
 
 	for i := 0; i < len(raw_data); i++ {
 		if raw_data[i] == 10 {
-			go checkLineParts(&raw_data, line_start, i, &sum)
-			wg.Add(1)
+			checkLineParts(&raw_data, line_start, i)
+
 			line_start = i + 1
 			line_count++
 		}
 		if i == len(raw_data)-1 {
-			go checkLineParts(&raw_data, line_start, i+1, &sum)
-			wg.Add(1)
+			checkLineParts(&raw_data, line_start, i+1)
+
 			line_count++
 		}
 	}
-	wg.Wait()
-	fmt.Println(sum.Value())
+	sum := 0
+	// not exactly 2, but still works
+	for i := 0; i < len(gears); i++ {
+		for j := 0; j < len(gears); j++ {
+			if gears[i] != nil && gears[j] != nil && gears[i].i == gears[j].i && i != j {
+				num := gears[i].num * gears[j].num
+				sum = sum + num
+				gears[j] = nil
+			}
+		}
 
+	}
+	fmt.Println(sum)
 }
 
-func checkLineParts(raw_data *[]byte, line_start int, line_end int, sum *SafeCounter) {
-	defer wg.Done()
+func checkLineParts(raw_data *[]byte, line_start int, line_end int) {
+	// defer wg.Done()
 	line_data := (*raw_data)[line_start:line_end]
+
 	// fmt.Println(line_data)
 	for i := 0; i < len(line_data); i++ {
 		if (line_data[i] < 48 || line_data[i] > 57) || i == len(line_data)-1 {
@@ -76,17 +72,21 @@ func checkLineParts(raw_data *[]byte, line_start int, line_end int, sum *SafeCou
 				num = num + int(math.Pow(10, pow))*int((line_data[j]-48))
 				pow++
 			}
-			if num > 0 && isConnectedNum(raw_data, j+1, i-1, line_start, line_end) {
-				fmt.Println(num)
-				sum.Inc(num)
+			if num > 0 {
+				if gear := isConnectedNum(raw_data, j+1, i-1, line_start, line_end, num); gear != nil {
+					// fmt.Println(gear)
+					// fmt.Println(num)
+					gears = append(gears, gear)
+				}
 
 			}
 
 		}
 	}
+	// fmt.Println(gears)
 }
 
-func isConnectedNum(raw_data *[]byte, start_idx int, end_idx int, line_start int, line_end int) bool {
+func isConnectedNum(raw_data *[]byte, start_idx int, end_idx int, line_start int, line_end int, num int) *Gear {
 	line_len := line_end - line_start
 	if start_idx > 0 {
 		start_idx--
@@ -99,26 +99,26 @@ func isConnectedNum(raw_data *[]byte, start_idx int, end_idx int, line_start int
 	if prev_start >= 0 {
 		for i := prev_start + start_idx; i <= prev_start+end_idx; i++ {
 			// fmt.Printf("%v %v\n", (*raw_data)[i], i)
-			if ((*raw_data)[i] < 48 || (*raw_data)[i] > 57) && (*raw_data)[i] != 46 {
-				return true
+			if (*raw_data)[i] == 42 {
+				return &Gear{i, num}
 			}
 		}
 	}
 	for i := line_start + start_idx; i <= line_start+end_idx; i++ {
 		// fmt.Printf("%v %v\n", (*raw_data)[i], i)
-		if ((*raw_data)[i] < 48 || (*raw_data)[i] > 57) && (*raw_data)[i] != 46 {
-			return true
+		if (*raw_data)[i] == 42 {
+			return &Gear{i, num}
 		}
 	}
 	next_start := line_start + line_len + 1
 	if next_start < len((*raw_data)) {
 		for i := next_start + start_idx; i <= next_start+end_idx; i++ {
 			// fmt.Printf("%v %v\n", (*raw_data)[i], i)
-			if ((*raw_data)[i] < 48 || (*raw_data)[i] > 57) && (*raw_data)[i] != 46 {
-				return true
+			if (*raw_data)[i] == 42 {
+				return &Gear{i, num}
 			}
 		}
 
 	}
-	return false
+	return nil
 }
