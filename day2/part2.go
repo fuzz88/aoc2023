206331ed23e34015c0d2d128fa07069ea493d10a
fuzz88 <ivan@oschepkov.ru>
2023-12-07 17:41:21 +0800

    add day2/part2.go

---
+++ day2/part2.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+)
+
+func checkPossible(start int, end int, raw_data *[]byte, sum chan int, powers chan int) {
+	//sends game number to sum channel if possible,
+	//or 0 when impossible
+
+	game_num := 0
+	possible := true
+	max_red := 0
+	max_green := 0
+	max_blue := 0
+	for i := start; i < end; i++ {
+		if (*raw_data)[i] == 58 {
+			pow := 0.0
+			for j := i - 1; (*raw_data)[j] != 32; j-- {
+				game_num = game_num + int(math.Pow(10, pow))*int(((*raw_data)[j]-48))
+				pow++
+			}
+		}
+
+		if (*raw_data)[i] == 59 || i == end-1 {
+			red := 0
+			green := 0
+			blue := 0
+
+			for j := i - 1; ((*raw_data)[j] != 58) && ((*raw_data)[j] != 59); j-- {
+				if (*raw_data)[j] == 98 {
+					pow := 0.0
+					k := j - 2
+					for ; (*raw_data)[k] != 32; k-- {
+						blue = blue + int(math.Pow(10, pow))*int(((*raw_data)[k]-48))
+						pow++
+					}
+				}
+				if (*raw_data)[j] == 114 {
+					pow := 0.0
+					k := j - 2
+					for ; (*raw_data)[k] != 32; k-- {
+						red = red + int(math.Pow(10, pow))*int(((*raw_data)[k]-48))
+						pow++
+					}
+				}
+				if (*raw_data)[j] == 103 {
+					pow := 0.0
+					k := j - 2
+					for ; (*raw_data)[k] != 32; k-- {
+						green = green + int(math.Pow(10, pow))*int(((*raw_data)[k]-48))
+						pow++
+					}
+				}
+			}
+			if !(red <= 12 && green <= 13 && blue <= 14) {
+				possible = false
+			}
+			if red > max_red {
+				max_red = red
+			}
+
+			if green > max_green {
+				max_green = green
+			}
+			if blue > max_blue {
+				max_blue = blue
+			}
+
+		}
+
+	}
+	if possible {
+		sum <- game_num
+	} else {
+		sum <- 0
+	}
+	powers <- max_blue * max_red * max_green
+
+}
+
+func main() {
+	sum := make(chan int, 100)
+	powers := make(chan int, 100)
+	raw_data, err := os.ReadFile("test2.txt")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("\n%v bytes read.\n", len(raw_data))
+	start := 0
+	end := 0
+	game_count := 0
+	for i := 0; i < len(raw_data); i++ {
+		if raw_data[i] == byte(10) {
+			end = i
+			go checkPossible(start, end, &raw_data, sum, powers)
+			game_count++
+			start = end + 1
+		}
+		if i == len(raw_data)-1 {
+			go checkPossible(start, i+1, &raw_data, sum, powers)
+			game_count++
+		}
+	}
+	acc := 0
+	for i := 0; i < game_count; i++ {
+		c := <-sum
+		acc = acc + c
+
+	}
+	fmt.Printf("\n%v\n", acc)
+
+	acc = 0
+	for i := 0; i < game_count; i++ {
+		c := <-powers
+		acc = acc + c
+
+	}
+	fmt.Printf("\n%v\n\n", acc)
+
+}
