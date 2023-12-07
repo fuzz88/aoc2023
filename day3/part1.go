74f00a739a632f9a21f0e42d52b095ee566d899f
fuzz88 <ivan@oschepkov.ru>
2023-12-07 21:18:17 +0800

    add some work

--- day3/part1.go
+++ day3/part1.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
@@ -10,20 +11,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	line_len := 0
-	line_num := 0
+	// fmt.Println(raw_data)
+	sum := make(chan int, 1000)
 	for i := 0; i < len(raw_data); i++ {
+		line_start := 0
 		if raw_data[i] == 10 || i == len(raw_data)-1 {
-			if line_len == 0 {
-				line_len = i
-			}
-			line_num++
-			fmt.Println(line_len, line_num)
-			checkLineParts(raw_data[(line_num-1)*line_len : line_num*line_len])
+			checkLineParts(raw_data[line_start:i], sum)
+			line_start = i
 		}
 	}
 }
 
-func checkLineParts(line_data []byte) {
-	fmt.Println(line_data)
+func checkLineParts(line_data []byte, sum chan int) {
+	fmt.Println(string(line_data))
+	for i := 0; i < len(line_data); i++ {
+		if (line_data[i] < 48 || line_data[i] > 57) || i == len(line_data)-1 {
+			pow := 0.0
+			num := 0
+			j := i - 1
+			// fmt.Printf("%v %v", line_data[i], i)
+			for ; j > -1 && line_data[j] >= 48 && line_data[j] <= 57; j-- {
+				num = num + int(math.Pow(10, pow))*int((line_data[j]-48))
+				pow++
+			}
+			if num > 0 && isConnectedNum(j+1, i-1) {
+				fmt.Println(num)
+				sum <- num
+			}
+
+		}
+	}
+}
+
+func isConnectedNum(start_idx int, end_idx int) bool {
+	fmt.Println(start_idx, end_idx)
+	return true
 }
