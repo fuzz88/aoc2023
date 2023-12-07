c2a99be12327dafe40e81cb2f6682d0ab7287d1c
fuzz88 <ivan@oschepkov.ru>
2023-12-07 20:46:39 +0800

    add parsing

--- day3/part1.go
+++ day3/part1.go
@@ -6,9 +6,24 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("test1.txt")
+	raw_data, err := os.ReadFile("test1.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(data)
+	line_len := 0
+	line_num := 0
+	for i := 0; i < len(raw_data); i++ {
+		if raw_data[i] == 10 || i == len(raw_data)-1 {
+			if line_len == 0 {
+				line_len = i
+			}
+			line_num++
+			fmt.Println(line_len, line_num)
+			checkLineParts(raw_data[(line_num-1)*line_len : line_num*line_len])
+		}
+	}
+}
+
+func checkLineParts(line_data []byte) {
+	fmt.Println(line_data)
 }
