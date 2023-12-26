c977a32a8c0aa0600e96c870a6c24a024fa751ce
fuzz88 <ivan@oschepkov.ru>
2023-12-26 12:12:54 +0800

    add day25 human+ai

---
+++ day24/particles.js
@@ -0,0 +1,21 @@
+
+/* 
+
+x, y, z, dx, dy, dz
+postion and velocity as a single array 
+
+*/
+
+let particles = [
+    [19, 13, 30, -2, 1, -2],
+    [18, 19, 22, -1, -1, -2],
+    [20, 25, 34, -2, -2, -4],
+    [12, 31, 28, 1, -5, -3],
+];
+
+
+console.log(particles)
+
+
+max_x = 10000000000
+max_y = 40000000000
