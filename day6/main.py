02f7136c9f3d183ac37cf880d48d55e1a629baaa
fuzz88 <ivan@oschepkov.ru>
2024-08-10 17:43:37 +0800

    add day6 python+numba solution

---
+++ day6/main.py
@@ -0,0 +1,77 @@
+from numba import jit
+
+
+def read_races_from_file(filename) -> list:
+    with open(filename) as f:
+        data = f.readlines()
+
+    times = list(map(int, data[0].split(":")[1].split()))
+    distances = list(map(int, data[1].split(":")[1].split()))
+
+    print(f"Times: {times}")
+    print(f"Distances: {distances}")
+
+    races = list(zip(times, distances))
+
+    print(f"Races: {races}")
+
+    return races
+
+
+def read_single_race_from_file(filename) -> tuple:
+    with open(filename) as f:
+        data = f.readlines()
+
+    race = (
+        int(
+            "".join(
+                [el for el in list(map(str.strip, data[0].split(":")[1])) if el != ""]
+            )
+        ),
+        int(
+            "".join(
+                [el for el in list(map(str.strip, data[1].split(":")[1])) if el != ""]
+            )
+        ),
+    )
+    print(f"input: {race}")
+    return race
+
+
+@jit
+def solvePart1(races) -> int:
+    result = 1
+    for race in races:
+        success_count = 0
+        for speed in range(0, race[0] + 1):
+            time = race[0] - speed
+            distance = speed * time
+            if distance > race[1]:
+                success_count = success_count + 1
+        result = result * success_count
+    return result
+
+
+@jit(nopython=True)
+def solvePart2(race) -> int:
+    success_count = 0
+    t_time = race[0]
+    t_dist = race[1]
+    for speed in range(0, race[0] + 1):
+        time = t_time - speed
+        distance = speed * time
+        if distance > t_dist:
+            success_count = success_count + 1
+    return success_count
+
+
+races = read_races_from_file("test1.txt")
+print(f"part1: {solvePart1(races)}")
+
+race = read_single_race_from_file("test2.txt")
+from time import monotonic
+
+start = monotonic()
+print(f"part2: {solvePart2(race)}")
+total = monotonic() - start
+print(f"part2 time: {total:.3f}s")
