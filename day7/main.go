2b5b4ce7b2cc425a1cc6063392394465214c2f3d
fuzz88 <ivan@oschepkov.ru>
2024-08-10 20:46:51 +0800

    add comment

--- day7/main.go
+++ day7/main.go
@@ -8,7 +8,7 @@ import (
 func main() {
 	fmt.Printf("\nAOC_2023 Day7 Solution\n\n")
 
-	args := os.Args[1:]
+	args := os.Args[1:] // skip program filename
 	for _, filePath := range args {
 		fmt.Println("input file:", filePath)
 	}
