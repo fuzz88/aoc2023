1b873163527f2c4f76bcd4b265af6241bc4b3a8a
fuzz88 <ivan@oschepkov.ru>
2024-08-10 17:30:56 +0800

    add Day6 part1 & part2

--- day6/main.go
+++ day6/main.go
@@ -8,14 +8,6 @@ import (
 	"strings"
 )
 
-func Map[T, V any](ts []T, fn func(T) V) []V {
-	result := make([]V, len(ts))
-	for i, t := range ts {
-		result[i] = fn(t)
-	}
-	return result
-}
-
 type Race struct {
 	time     int
 	distance int
@@ -52,12 +44,55 @@ func read_data_from_file(filePath string) ([]Race, error) {
 	return results, nil
 }
 
-func solvePart1(races []Race) int {
-	return 0
+func read_single_race_from_file(filePath string) ([]Race, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var data []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		nums := strings.Fields(text)
+		s := ""
+		for _, num := range nums[1:] {
+			s = s + num
+		}
+		value, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, err
+		}
+		data = append(data, value)
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	var results []Race
+	results = append(results, Race{time: data[0], distance: data[1]})
+	return results, nil
+}
+
+func solve(races []Race) int {
+	result := 1
+	for i := 0; i < len(races); i++ {
+		success_count := 0
+		for speed := 0; speed < races[i].time; speed++ {
+			time := races[i].time - speed
+			distance := speed * time
+			if distance > races[i].distance {
+				success_count++
+			}
+		}
+		result = result * success_count
+	}
+	return result
 }
 
 func main() {
-	fmt.Printf("\nAOC_2023 Day6 Solution\n")
+	fmt.Printf("\nAOC_2023 Day6 Solution\n\n")
 
 	args := os.Args[1:]
 	for _, filePath := range args {
@@ -66,6 +101,12 @@ func main() {
 			fmt.Println(err)
 		}
 		fmt.Printf("input: %v\n", data)
-		fmt.Printf("Part 1: %d\n\n", solvePart1(data))
+		fmt.Printf("Part 1: %d\n\n", solve(data))
+		data, err = read_single_race_from_file(filePath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Printf("input: %v\n", data)
+		fmt.Printf("Part 2: %d\n\n", solve(data))
 	}
 }
