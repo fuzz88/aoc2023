2992b8dfa54a1b911ea9b2cff9dd7aaa31da7588
fuzz88 <ivan@oschepkov.ru>
2024-08-10 20:21:42 +0800

    add day7 start

---
+++ day7/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("\nAOC_2023 Day7 Solution\n\n")
+
+	args := os.Args[1:]
+	for _, filePath := range args {
+		fmt.Println("input file:", filePath)
+	}
+}
