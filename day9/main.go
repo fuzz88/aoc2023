7992b7fc368455dc080158f91be248a7970b4464
fuzz88 <ivan@oschepkov.ru>
2024-08-19 20:39:41 +0800

    day9 'read arguments'

--- day9/main.go
+++ day9/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Printf("\nAOC-2023 Day9 Solution\n\n")
+	args := os.Args[1:]
+	for _, filePath := range args {
+		fmt.Println(filePath)
+	}
 }
