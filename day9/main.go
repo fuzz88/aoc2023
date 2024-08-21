31df152ed164c3c9d8ae9be9b6502315a8851196
fuzz88 <ivan@oschepkov.ru>
2024-08-21 22:23:16 +0800

    [day9] Reading seqs from input files

--- day9/main.go
+++ day9/main.go
@@ -1,14 +1,48 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+func readSeqFromFile(filePath string) [][]int {
+	file, err := os.Open(filePath)
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+
+	var result [][]int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		var numbers []int
+		for _, item := range fields {
+			num, err := strconv.Atoi(item)
+			if err != nil {
+				panic(err)
+			}
+			numbers = append(numbers, num)
+		}
+		result = append(result, numbers)
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	return result
+}
 
 func main() {
 	fmt.Printf("\nAOC-2023 Day9 Solution\n\n")
 	args := os.Args[1:]
 	for _, filePath := range args {
 		fmt.Println(filePath)
+		fmt.Println(len(readSeqFromFile(filePath)))
 	}
 }
