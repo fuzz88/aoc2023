b5fea84ee26a64aca1ff9d858f63ba81482df3f3
fuzz88 <ivan@oschepkov.ru>
2024-08-21 23:53:45 +0800

    [day9] works on sample but not on test

--- day9/main.go
+++ day9/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -38,11 +39,38 @@ func readSeqsFromFile(filePath string) [][]int {
 	return result
 }
 
+func getNextValueForSeq(seq []int) int {
+	var sub_seq []int
+	dx := seq[0] - seq[1]
+	if dx == 0 {
+		return seq[0]
+	}
+	for i := 0; i < len(seq)-1; i++ {
+		sub_seq = append(sub_seq, seq[i]-seq[i+1])
+	}
+	fmt.Println(sub_seq)
+	return seq[0] + getNextValueForSeq(sub_seq)
+
+}
+
+func solve(seqs [][]int) int {
+	result := 0
+	for _, seq := range seqs {
+		slices.Reverse(seq)
+		fmt.Println(seq)
+		value := getNextValueForSeq(seq)
+		fmt.Println(value)
+		result = result + value
+	}
+	return result
+}
+
 func main() {
 	fmt.Printf("\nAOC-2023 Day9 Solution\n\n")
 	args := os.Args[1:]
 	for _, filePath := range args {
 		fmt.Println(filePath)
-		fmt.Println(len(readSeqsFromFile(filePath)))
+		seqs := readSeqsFromFile(filePath)
+		fmt.Println(solve(seqs))
 	}
 }
