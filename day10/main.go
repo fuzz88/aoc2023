d1401793ef6e9de7f10efea333578979c340d83e
fuzz88 <ivan@oschepkov.ru>
2024-08-22 10:24:52 +0800

    [day10] Read arguments

---
+++ day10/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Printf("\nAOC-2023 Day10 Solution\n\n")
+	
+	args := os.Args[1:]
+	for _, filePath := range args {
+		fmt.Println(filePath)
+	}
+}
