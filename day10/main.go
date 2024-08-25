32549ccccc3365ad179897c9fdaa4ee38ebe6093
fuzz88 <ivan@oschepkov.ru>
2024-08-25 23:01:26 +0800

    [day10] part2 unsolved(

--- day10/main.go
+++ day10/main.go
@@ -91,6 +91,108 @@ func solve(surface Surface) int {
 	return ((slices.Max(distances) + 1) / 2)
 }
 
+func solve2(surface Surface) int {
+	height := len(surface)
+	width := len(surface[0])
+	checkBounds := func(row int, col int) bool {
+		if row < 0 || col < 0 || row > height-1 || col > width-1 {
+			return false
+		} else {
+			return true
+		}
+	}
+	inLoop := func(row int, col int) bool {
+		meet := 0
+		for i := row; checkBounds(i, col); i++ {
+			if surface[i][col] == []rune("V")[0] {
+				meet++
+				break
+			}
+		}
+		for i := row; checkBounds(i, col); i-- {
+			if surface[i][col] == []rune("V")[0] {
+				meet++
+				break
+			}
+		}
+		for i := col; checkBounds(row, i); i++ {
+			if surface[row][i] == []rune("V")[0] {
+				meet++
+				break
+			}
+		}
+		for i := col; checkBounds(row, i); i-- {
+			if surface[row][i] == []rune("V")[0] {
+				meet++
+				break
+			}
+		}
+		if meet < 4 {
+			return false
+		} else {
+			return true
+		}
+	}
+	var walkNextStepAndMarkOutside func(row int, col int)
+
+	walkNextStepAndMarkOutside = func(row int, col int) {
+		if checkBounds(row - 1, col) {
+			if surface[row - 1][col] == []rune(".")[0] {
+				surface[row - 1][col] = []rune("O")[0]
+				walkNextStepAndMarkOutside(row - 1, col)
+			}
+		}
+		if checkBounds(row + 1, col) {
+			if surface[row + 1][col] == []rune(".")[0] {
+				surface[row + 1][col] = []rune("O")[0]
+				walkNextStepAndMarkOutside(row - 1, col)
+			}
+		}
+		if checkBounds(row, col - 1) {
+			if surface[row][col - 1] == []rune(".")[0] {
+				surface[row][col - 1] = []rune("O")[0]
+				walkNextStepAndMarkOutside(row, col - 1)
+			}
+		}
+		if checkBounds(row, col + 1) {
+			if surface[row][col + 1] == []rune(".")[0] {
+				surface[row][col + 1] = []rune("O")[0]
+				walkNextStepAndMarkOutside(row, col + 1)
+			}
+		}
+	}
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			if !inLoop(row, col) && surface[row][col] != []rune("O")[0] {
+				surface[row][col] = []rune("O")[0]
+				walkNextStepAndMarkOutside(row, col)
+			}
+		}
+	}
+	count := 0
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+		if surface[row][col] == []rune(".")[0] {
+			count++	
+			}
+		}
+	}
+
+	return count
+}
+
+func cleanSurface(surface Surface) {
+	height := len(surface)
+	width := len(surface[0])
+	for row := 0; row < height; row++ {
+		for col := 0; col < width; col++ {
+			if surface[row][col] != []rune("V")[0] {
+				surface[row][col] = []rune(".")[0]
+			}
+		}
+	}
+}
+
 func main() {
 	fmt.Printf("\nAOC-2023 Day10 Solution\n\n")
 
@@ -98,6 +200,14 @@ func main() {
 	for _, filePath := range args {
 		fmt.Println(filePath)
 		surface := readSurfaceFromFile(filePath)
+		for i:=0; i < len(surface); i++ {
+			fmt.Println(string(surface[i]))
+		}
 		fmt.Println("Part1: ", solve(surface))
+		cleanSurface(surface)
+		for i:=0; i < len(surface); i++ {
+			fmt.Println(string(surface[i]))
+		}
+		fmt.Println("Part2: ", solve2(surface))
 	}
 }
