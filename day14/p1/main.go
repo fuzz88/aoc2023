40e7d4b1f237270bfc1716571c021d8634186a03
fuzz88 <ivan@oschepkov.ru>
2025-07-10 15:05:30 +0800

    [day14] p1 solved

--- day14/p1/main.go
+++ day14/p1/main.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
 )
 
-func readInput(filename string) (int, []byte) {
+func readInput(filename string) (int, int, []byte) {
 	file, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -23,7 +24,7 @@ func readInput(filename string) (int, []byte) {
 
 	data := make([]byte, 0, fileSize)
 	var scanner = bufio.NewScanner(file)
-	
+
 	lineCount := 0
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -31,7 +32,71 @@ func readInput(filename string) (int, []byte) {
 		lineCount++
 	}
 
-	return int(fileSize / int64(lineCount) - 1), data
+	return int(fileSize/int64(lineCount) - 1), lineCount, data
+}
+
+func makeColsChan(field *[]byte, cols int, rows int) chan []byte {
+	output := make(chan []byte)
+
+	go func() {
+		result := make([][]byte, cols)
+
+		for col := 0; col < cols; col++ {
+			result[col] = make([]byte, 0, rows)
+		}
+
+		for i := 0; i < len(*field); i++ {
+			col := i % cols
+			result[col] = append(result[col], (*field)[i])
+		}
+
+		for col := 0; col < cols; col++ {
+			output <- result[col]
+		}
+		close(output)
+	}()
+
+	return output
+}
+
+func countRocksNorth(field *[]byte, cols int, rows int) int {
+	results := make(chan int)
+
+	var wg sync.WaitGroup
+
+	for col := range makeColsChan(field, cols, rows) {
+
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			scores := 0
+			score := rows
+			for idx, item := range col {
+				if item == 'O' {
+					scores += score
+					score -= 1
+				}
+				if item == '#' {
+					score = rows - (idx + 1)
+				}
+			}
+			// fmt.Println(col, scores)
+			results <- scores
+
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	sum := 0
+	for r := range results {
+		sum += r
+	}
+	return sum
 }
 
 func main() {
@@ -43,10 +108,8 @@ func main() {
 	}
 
 	inputFile := os.Args[1]
-	lineLength, inputData := readInput(inputFile)
+	cols, rows, field := readInput(inputFile)
 
-	fmt.Println(inputFile)
-	fmt.Println(inputData)
-	fmt.Println("Line length:", lineLength)
+	fmt.Println(countRocksNorth(&field, cols, rows))
 
 }
